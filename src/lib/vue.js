3448bf7d59e97bd4d41d184ee827f892124a61d1
beautifulBoys <1298947916@qq.com>
2017-10-28 12:05:42 +0800

    a

--- src/lib/vue.js
+++ src/lib/vue.js
@@ -1,12 +1,38 @@
 import Router from '../router/index.js';
 
-function init () {
+function getActiveComponent (router) {
+  let act;
+  for (let i = 0; i < router.length; i++) {
+    if (router[i].active) {
+      act = router[i].commponent;
+      break;
+    }
+  }
+  return act;
+}
 
+function init (router) {
+  let active = getActiveComponent(router);
+
+  if (active.beforeCreate) active.beforeCreate();
+  if (active.created) active.created();
+  if (active.beforeMount) active.beforeMount();
+  if (active.mounted) active.mounted();
+  if (active.beforeUpdate) active.beforeUpdate();
+  if (active.updated) active.updated();
+  if (active.beforeDestroy) active.beforeDestroy();
+  if (active.destroyed) active.destroyed();
+
+  return router;
 }
 
 const Vue = ({el, router, store}) => {
-  Router(router);
-  
+  if (!el || !router) {
+    console.log('参数错误');
+    return;
+  }
+  router = init(router);
+  Router(router, el);
 };
 
 export default Vue;
--- src/router/index.js
+++ src/router/index.js
@@ -1,5 +1,11 @@
-export default Router (router) {
-  for (let i = 0; i < router.length; i++) {
-    
-  }
+function getElement (el) {
+  return document.getElementById(el.substr(1));
 }
+
+export default function Router (routers, el) {
+  for (let i = 0; i < routers.length; i++) {
+    if (routers[i].active) {
+      getElement(el).innerHTML = routers[i].commponent.template;
+    }
+  }
+};
